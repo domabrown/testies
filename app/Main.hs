f917490864e73c47a060b8cfc597098a52163601
domabrown <dom-doi@live.co.uk>
2022-06-17 08:29:14 +0100

    update with Advent day 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,11 @@
 
 import Lib
 import Data.List
-
+{-
 main :: IO ()
 main = do
   x <- readFile "input.txt"
-  print . reduce . solve . parseInput $ x 
+  print . reduce . solve . solve' . parseInput $ x 
 
 parseInput :: String -> [Int]
 parseInput = map read . lines
@@ -14,12 +14,70 @@ data Change = Increase | Decrease
   deriving (Eq, Show) 
 
 solve :: [Int] -> [Change]
-solve [] = []
-solve [x] = []
-solve (x : y : zs) = (if (y > x ) then Increase else Decrease) : solve (y : zs)  
+solve (x : y : z) = (if (y > x ) then Increase else Decrease) : solve (y : z)
+solve _ = []
+
+solve' :: [Int] -> [Int]
+solve' (x : y : z : xs) = (x + y + z) : solve' (y : z : xs)  
+solve' _ = []
 
 reduce :: [Change] -> Int
-reduce x = length $ filter (== Increase) x
+reduce = length . filter (Increase ==)
+-}
+
+
+
+
+
+
+{- get data from each direction, up n, down n, forward n    could possibly add up/down on own then minus down from up? ()
+So we want, Read file, Add all forward, take away all down from up 
+-}
+submarine :: IO ()
+submarine = do 
+  a <- readFile "submarine.txt"
+  print . solve
+
+
+parseInput :: String -> submarineDir Int
+parseInput = map read . lines  --- I want the dir + the int
+
+type Depth = Int     -- verti movement the 2 values I want added
+type Movement = Int  -- hori movement
+
+data SubmarineDir =  Forward Int | Up Int | Down Int    -- the directions 
+  deriving (Eq, Show)
+
+SubmarineDir = dist . words
+  where 
+   dist ["forward", x] = Forward (read x) 
+   dist ["up", x] = Up (read x)
+   dist ["down", x] = Down (read x)
+
+
+direction :: [SubmarineDir] -> (Depth , Movement)
+direction (Forward , x) _ = (0 + x)
+direction (Up , x)      _ = (x + x)
+direction (Down , x )    _ = (x - x)
+
+ --solve2 :: [Int] -> SubmarineDir
+ -- solve2  = Movement * Depth 
+
+
+
+--if submarineDir = "forward"
+--moveForward :: [Char] -> [Int]
+
+--if/else for up/down if up add else minus
+
+
+
+
+
+-- break down so add three at a  time at first? then go from them? 
+
+
+listTimes2 = [x * 2 | x <- [1..20]]
 
 
 doubleMe x = x + x
